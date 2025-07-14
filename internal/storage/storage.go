d3e051c8690e9a37763e2942caba0de9fdba9407
Sher Shah Suri <shershahsuri59@gmail.com>
2025-07-14 21:46:23 +0500

    added the deleteStudentByID method into the interface to be implemented on the database

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -7,4 +7,5 @@ type Storage interface {
 	GetStudentByID(id int64) (types.Student, error)
 	GetStudents() ([]types.Student, error)
 	UpdateStudentByID(id int64, name string, email string, age int) (int64, error)
+	DeleteStudentByID(id int64) (int64, error)
 }
