5375d65074de5934d8db75a9a332a1bb6293733e
Sher Shah Suri <shershahsuri59@gmail.com>
2025-07-14 21:22:34 +0500

    RestAPI in Golang (net/http) with Get, Post, Patch requests so far. Using SQLite for database and production level code structure. Next Steps: Add Delete request, Integrate DBs like MySQL, PostgreSQL.

---
+++ cmd/server/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/codingsher/golang-student-api/internal/config"
+	"github.com/codingsher/golang-student-api/internal/http/handlers/student"
+	"github.com/codingsher/golang-student-api/internal/storage/sqlite"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	storage, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage initializated", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("POST /api/students", student.New(storage))
+	router.HandleFunc("GET /api/students/{id}", student.GetByID(storage))
+	router.HandleFunc("GET /api/students", student.GetList(storage))
+	router.HandleFunc("PATCH /api/students/{id}", student.UpdateByID(storage))
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("server failed to start!")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("serve failed to shutdown", slog.String("error", err.Error()))
+	}
+
+	slog.Info("server shutdown success...")
+}
